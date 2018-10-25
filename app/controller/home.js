21a225ab2ee081e12509459df115b6a7bcd18769
lidaibin <lidaibin@cmcm.com>
2018-10-25 10:48:36 +0800

    feature: add token get function

--- app/controller/home.js
+++ app/controller/home.js
@@ -3,29 +3,34 @@
 const sha1 = require('sha1');
 const Controller = require('egg').Controller;
 
-const token = "lidaibin";
+const token = 'lidaibin';
 class HomeController extends Controller {
   async index() {
     this.ctx.body = 'hi, egg';
   }
   async wxtoken() {
-    console.log("验证token");
+    console.log('验证token');
     const query = this.ctx.query;
-    console.log("参数", query);
-    const {signature, nonce, timestamp, echostr} = query;
-    const array = [nonce, timestamp, token];
+    console.log('参数', query);
+    const { signature, nonce, timestamp, echostr } = query;
+    const array = [ nonce, timestamp, token ];
     array.sort();
-    const str = array.join("");
+    const str = array.join('');
     const sign = sha1(str);
-    console.log("对比签名", sign, signature);
+    console.log('对比签名', sign, signature);
     if (sign === signature) {
       this.ctx.body = echostr;
     } else {
       this.ctx.body = null;
     }
   }
-  async getTaoken() {
-
+  async getwxtoken() {
+    const { nonce, timestamp } = this.ctx.query;
+    const array = [ nonce, timestamp, token ];
+    array.sort();
+    const str = array.join('');
+    const sign = sha1(str);
+    this.ctx.body = sign;
   }
 }
 
