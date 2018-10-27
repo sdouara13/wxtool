fcd92127a422012d6fd93936701ee6fc83e37ff4
lidaibin <lidaibin@cmcm.com>
2018-10-27 09:38:33 +0800

    feature: fix access token api

--- app/controller/home.js
+++ app/controller/home.js
@@ -1,9 +1,11 @@
 'use strict';
 
 const sha1 = require('sha1');
+const request = require('request-promise');
 const Controller = require('egg').Controller;
 
 const token = 'lidaibin';
+const config = require('../../config/wx');
 class HomeController extends Controller {
   async index() {
     this.ctx.body = 'hi, egg';
@@ -26,6 +28,26 @@ class HomeController extends Controller {
   }
   async getWXtoken() {
     const { nonce, timestamp } = this.ctx.query;
+    const options = {
+      uri: 'https://api.weixin.qq.com/cgi-bin/token',
+      qs: {
+        grant_type: 'client_credential',
+        appid: config.appid,
+        secret: config.secret,
+      },
+      headers: {
+        'User-Agent': 'Request-Promise',
+      },
+      json: true,
+    };
+    request(options)
+      .then(res => {
+        console.log('申请accesstoken结果', res);
+      })
+      .catch(err => {
+        console.log('申请accesstoken失败', err);
+      })
+    // request.get(`https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${config.appid}&secret=${config.secret}`);
     const array = [ nonce, timestamp, token ];
     array.sort();
     const str = array.join('');
---
+++ config/wx.js
@@ -0,0 +1,6 @@
+'use strict';
+
+module.exports = {
+  appid: 'wxaca505e7b260c383',
+  secret: '2d0031fbdea1d833b1609130e75a3e94',
+}
